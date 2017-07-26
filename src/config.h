bb2b3184fa1f521dfe8b6f295e2a2f48ae130c58
hotmori <hotmori@gmail.com>
2017-07-26 17:13:33 +0300

    Revert "Snake battle show messages"

--- src/config.h
+++ src/config.h
@@ -9,9 +9,6 @@ static const unsigned GO_DOWN = 1;
 static const unsigned GO_LEFT = 2;
 static const unsigned GO_RIGHT = 3;
 
-static const unsigned FIRST_PLAYER_ID = 1;
-static const unsigned SECOND_PLAYER_ID = 2;
-
 static const unsigned CELL_SIZE = 10;
 static const unsigned CELL_WIDTH = WINDOW_WIDTH / CELL_SIZE;
 static const unsigned CELL_HEIGHT = WINDOW_HEIGHT / CELL_SIZE;
@@ -21,15 +18,4 @@ static const unsigned START_Y = CELL_HEIGHT / 2;
 static const unsigned START_LENGTH = 4;
 
 static const unsigned RENDERING_TIMEOUT = 5;
-
-//Text messages
-static const unsigned MSG_MAX_COUNT = 10;
-static const char MSG_FONT[] = "FreeSans.ttf";
-static const unsigned MSG_FONT_SIZE = 24;
-static const unsigned MSG_GAME_START = 0;
-static const unsigned MSG_GAME_OVER = 1;
-static const unsigned MSG_FIRST_PLAYER_WIN = 2;
-static const unsigned MSG_SECOND_PLAYER_WIN = 3;
-static const unsigned MSG_GAME_DRAW = 4;
-
 
